2eda34f69451c46c78621803c76c9cb616f1a80b
Krzysztof Podlaski <podlaski@uni.lodz.pl>
2017-11-30 12:18:06 +0100

    Bariera i semafor 30.11

---
+++ BarierExample/Horse.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BarierExample {
+    class Horse : IComparable<Horse>{
+        public static int Distance { get; set; }
+        public static Random rand = new Random();
+        private double position = 0.0;
+        private Barrier barierStart, barierMeta;
+        private double v;
+        public int Time { get; set; }
+        public int Id;
+        private static int lastId=1;
+        private Thread t;
+        public Semaphore kontrola { get; set; }
+
+        public Horse(Barrier bS, Barrier bM) {
+            lock (rand) {
+                v = rand.NextDouble();
+                Id = lastId++;
+            }
+            Time = 0;
+            barierStart = bS;
+            barierMeta = bM;
+            t = new Thread(Run);
+        }
+
+        public void Start() {
+            t.Start();
+        }
+
+        public void Run() {
+            Time = 0;
+            barierStart.SignalAndWait();
+            Console.WriteLine("Koń startuje");
+            while (position < Distance) {
+                position += v;
+                Time++;
+            }
+            Console.WriteLine("Koń na mecie");
+            barierMeta.SignalAndWait();
+            Console.WriteLine("Czekam na kontrole "+Id);
+            kontrola.WaitOne();
+            Console.WriteLine("Trwa kontrola " + Id);
+            Thread.Sleep(1000);
+            kontrola.Release();
+            Console.WriteLine("Koń "+Id+" idzie do stajni");
+        }
+
+        public int CompareTo(Horse other) {
+           return  Time - other.Time;
+        }
+    }
+}
---
+++ BarierExample/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BarierExample {
+    class Program {
+        static void Main(string[] args) {
+            Horse.Distance = 10000;
+            List<Horse> horse = new List<Horse>();
+            Barrier barrierStart = new Barrier(6,
+                (b) => {
+                    Console.WriteLine("START!!!");                    
+                });
+            Barrier barrierMeta = new Barrier(6,
+                (b) => {
+                    Console.WriteLine("Wyniki:");
+                    horse.Sort();
+                    foreach (Horse h in horse) {
+                        Console.WriteLine(h.Id + "\t" + h.Time + "[s]");
+                    }
+                    Thread.Sleep(1000);
+                });
+            Semaphore kontrola = new Semaphore(2,2);
+            for (int i=0; i<5; i++) {
+                horse.Add(new Horse(barrierStart,barrierMeta));
+            }
+            for (int i = 0; i < horse.Count; i++) {
+                horse[i].kontrola = kontrola;
+                horse[i].Start();
+            }
+            barrierStart.SignalAndWait();
+            barrierMeta.SignalAndWait();
+            Console.WriteLine("Koniec Części z Barierą");
+            Console.WriteLine("Kontrola Antydopingowa");
+            Console.ReadKey();
+        }
+    }
+}
